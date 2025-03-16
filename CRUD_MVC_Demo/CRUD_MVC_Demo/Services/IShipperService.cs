37fc285b45a84c6ec8959810a094ea0ac5bbe9ac
雨塵 <jer1357900@gmail.com>
2025-03-17 07:56:42 +0800

    增加商業邏輯

---
+++ CRUD_MVC_Demo/CRUD_MVC_Demo/Services/IShipperService.cs
@@ -0,0 +1,14 @@
+﻿using CRUD_MVC_Demo.Models;
+
+public interface IShipperService
+{
+    Task<IEnumerable<Shipper>> GetAllShippersAsync();
+
+    Task<Shipper> GetShipperByIdAsync(int id);
+
+    Task AddShipperAsync(Shipper shipper);
+
+    Task UpdateShipperAsync(Shipper shipper);
+
+    Task DeleteShipperAsync(int id);
+}
---
+++ CRUD_MVC_Demo/CRUD_MVC_Demo/Services/ShipperService.cs
@@ -0,0 +1,36 @@
+﻿using CRUD_MVC_Demo.Models;
+
+public class ShipperService : IShipperService
+{
+    private readonly IShipperRepository _repository;
+
+    public ShipperService(IShipperRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<IEnumerable<Shipper>> GetAllShippersAsync()
+    {
+        return await _repository.GetAllAsync();
+    }
+
+    public async Task<Shipper> GetShipperByIdAsync(int id)
+    {
+        return await _repository.GetByIdAsync(id);
+    }
+
+    public async Task AddShipperAsync(Shipper shipper)
+    {
+        await _repository.AddAsync(shipper);
+    }
+
+    public async Task UpdateShipperAsync(Shipper shipper)
+    {
+        await _repository.UpdateAsync(shipper);
+    }
+
+    public async Task DeleteShipperAsync(int id)
+    {
+        await _repository.DeleteAsync(id);
+    }
+}
